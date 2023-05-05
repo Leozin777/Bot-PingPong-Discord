0ffe4431d2d29653b2cf20decb1cc37dd274e96b
leozin777 <leo.cardoso@rede.ulbra.br>
2023-05-05 00:19:54 -0300

    bot recebe comando vazio

---
+++ TestBotTutorialDoYoutube/Modules/Command.cs
@@ -0,0 +1,12 @@
+﻿using Discord.Commands;
+
+namespace TestBotTutorialDoYoutube.Modules;
+
+public class Command : ModuleBase<SocketCommandContext>
+{
+    [Command("ping")]
+    public async Task Ping()
+    {
+        await ReplyAsync("Pong");
+    }
+}
---
+++ TestBotTutorialDoYoutube/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
+
+class Program 
+{
+    static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+
+    private DiscordSocketClient _client;
+    private CommandService _commands;
+    private IServiceProvider _serviceProvider;
+
+    public async Task RunBotAsync()
+    {
+        _client = new DiscordSocketClient();
+        _commands = new CommandService();
+
+        _serviceProvider = new ServiceCollection()
+            .AddSingleton(_client)
+            .AddSingleton(_commands)
+            .BuildServiceProvider();
+
+        string token = "MTEwMzg2NDc0NjE5NDc4MDIwMA.GpnFhA.PyrZZJEsp8270lxI5YL3AL1qJjOmJ5DOrjTgCY";
+        _client.Log += _client_Log;
+
+        await RegisterCommandsAsync();
+
+        await _client.LoginAsync(TokenType.Bot, token);
+
+        await _client.StartAsync();
+
+        await Task.Delay(-1);
+    }
+
+    private Task _client_Log(LogMessage arg)
+    {
+        Console.WriteLine(arg);
+        return Task.CompletedTask;
+    }
+
+    public async Task RegisterCommandsAsync()
+    {
+        _client.MessageReceived += HandleCommandAsync;
+        await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _serviceProvider);
+    }
+
+    private async Task HandleCommandAsync(SocketMessage arg)
+    {
+        var message = arg as SocketUserMessage;
+        var context = new SocketCommandContext(_client, message);
+        if (message.Author.IsBot) return;
+
+        int argPos = 0;
+        if (message.HasStringPrefix("!", ref argPos))
+        {
+            var result = await _commands.ExecuteAsync(context, argPos, _serviceProvider);
+            if (!result.IsSuccess) Console.WriteLine(result.ErrorReason);
+            if (result.Error.Equals(CommandError.UnmetPrecondition)) await message.Channel.SendMessageAsync(result.ErrorReason);
+        }
+    }
+}
+
+
+
